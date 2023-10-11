8bdc0983eaacfd83d02d79279262ea2a9a995759
judicael <judicael.poumay@gmail.com>
2023-10-11 20:07:39 +0200

    READY FOR COURSE

--- Assets/Scripts/SkeletonAI/EnemyManager.cs
+++ Assets/Scripts/SkeletonAI/EnemyManager.cs
@@ -2,39 +2,15 @@ using System.Collections.Generic;
 using BehaviorTree;
 using System;
 using System.Threading.Tasks;
+using UnityEngine;
 
-public class EnemyManager : Tree
+public class EnemyManager : MonoBehaviour
 {    
-    public UnityEngine.Transform[] waypoints;
     
     public static int healthpoints = 60; 
-    public static float speed = 1f;
-    public static bool frozen = false;
-    public static float fovRange = 3f;
-
-    protected override Node SetupTree()
-    {
-        Node root = new Selector(new List<Node>
-        {
-            new Sequence(new List<Node>
-            {
-                new CheckGuardInFOVRange(transform, fovRange),
-                new RunAway(transform,speed),
-            }),
-            new TaskPatrol1(transform, waypoints, speed),
-        });
-
-        return root;
-    }
 
     public bool TakeHit()
     {        
-        frozen = true;
-        Task.Run(async () => {
-            await Task.Delay(300);
-            frozen = false;
-        });
-
         healthpoints -= 10;
         bool isDead = healthpoints <= 0;
         if (isDead) _Die();
